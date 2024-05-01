54bb61ff88b06eb1d1e3eb06782b90601d4123d6
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-01 18:01:08 +0530

    feat: sort, filter and search added

--- src/app/case-handler/case-handler.module.ts
+++ src/app/case-handler/case-handler.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { CaseHandlerRoutingModule } from './case-handler-routing.module';
 import { CaseListComponent } from './case-list/case-list.component';
 import { SharedModule } from '../shared/shared.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     CaseHandlerRoutingModule,
-    SharedModule
+    SharedModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class CaseHandlerModule { }
