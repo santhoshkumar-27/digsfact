2de94058bc82faeb73f9e06a50233eabfbe32003
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-01 15:20:25 +0530

    feat: added header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,13 +4,15 @@ import { NavComponent } from './nav/nav.component';
 import { NgOptimizedImage } from '@angular/common'
 import {MatIconModule} from '@angular/material/icon';
 import { UserProfileViewComponent } from './user-profile-view/user-profile-view.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
     NavComponent,
-    UserProfileViewComponent
+    UserProfileViewComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
@@ -19,7 +21,8 @@ import { UserProfileViewComponent } from './user-profile-view/user-profile-view.
   ],
   exports: [
     NavComponent,
-    UserProfileViewComponent
+    UserProfileViewComponent,
+    HeaderComponent
   ]
 })
 export class SharedModule { }
