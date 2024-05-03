0a9234a2852e959309e40c3ef2b40bd76c782c40
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-03 07:23:07 +0530

    feat: interface written

---
+++ src/app/shared/interface.ts
@@ -0,0 +1,8 @@
+export interface CaseList {
+    customerName: string;
+    customerID: number;
+    customerEmail: string;
+    customerPhone: number;
+    caseID: string;
+    createdAt: number;
+}
