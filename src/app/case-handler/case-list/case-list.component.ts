4274a3cc8ebcb5823a0617163b6244d8be813492
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-03 07:24:02 +0530

    fix: removed unwanted codes

--- src/app/case-handler/case-list/case-list.component.ts
+++ src/app/case-handler/case-list/case-list.component.ts
@@ -5,27 +5,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { SelectionModel } from "@angular/cdk/collections";
 import { CustomPaginator } from 'src/app/shared/customlabelpaginator';
 import { MatPaginatorIntl } from '@angular/material/paginator';
-
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: "Hydrogen", weight: 1.0079, symbol: "H" },
-  { position: 2, name: "Helium", weight: 4.0026, symbol: "He" },
-  { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
-  { position: 4, name: "Beryllium", weight: 9.0122, symbol: "Be" },
-  { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
-  { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
-  { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "N" },
-  { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
-  { position: 9, name: "Fluorine", weight: 18.9984, symbol: "F" },
-  { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" }
-];
+import { CaseList } from 'src/app/shared/interface';
 
 @Component({
   selector: 'app-case-list',
@@ -48,8 +28,8 @@ export class CaseListComponent {
     "joinWith",
     "symbol"
   ];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  selection = new SelectionModel<PeriodicElement>(true, []);
+  dataSource = new MatTableDataSource<CaseList>([]);
+  selection = new SelectionModel<CaseList>(true, []);
 
   /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
